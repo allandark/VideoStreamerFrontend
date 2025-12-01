5d5ddfdee8ddf9aebade2d259f8dbf08f9c60134
Dennis Amtoft Jensen <dennis.jensen@specialisterne.dk>
2025-12-01 09:17:52 +0100

    force line endings to follow linux style LF

--- React/Frontend/entry.sh
+++ React/Frontend/entry.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
-set -e 
+set -e
 echo "starting nginx server"
-envsubst '${SERVER_NAME} ${API_SERVER_NAME} ${PORT}' \
-    < /etc/nginx/nginx.conf.template \
-    > /etc/nginx/nginx.conf
-
+echo "${SERVER_NAME}:${PORT}"
+export DOLLAR="$"
+# envsubst '${SERVER_NAME} ${API_SERVER_NAME} ${PORT}' \
+#     < /etc/nginx/nginx.conf.template \
+#     > /etc/nginx/nginx.conf
+envsubst < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+cat /etc/nginx/nginx.conf
 nginx -t
 exec nginx -g 'daemon off;'
